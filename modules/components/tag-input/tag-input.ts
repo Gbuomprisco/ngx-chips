5b7d8139c8f3b478e510de842e0de4350c484615
Eyal Hakim <eyaljhakim@gmail.com>
2017-06-15 19:30:18 +0300

    oops

--- modules/components/tag-input/tag-input.ts
+++ modules/components/tag-input/tag-input.ts
@@ -917,7 +917,7 @@ export class TagInputComponent extends TagInputAccessor implements OnInit, After
             .of(this.getItemDisplay(item))
             .map(display => this.setInputValue(display))
             .filter(display => this.inputForm.form.valid && !!display)
-            .map((display: string) => this.createTag(item))
+            .map((display: string) => this.createTag(fromAutocomplete ? item : display))
             .filter(validationFilter)
             .subscribe(appendItem, undefined, reset);
     }
